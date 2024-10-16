c6521e9b6c5a86f5ebcc0c75820a02d91628058f
Cédric Scherer <cedricphilippscherer@gmail.com>
2024-10-16 14:10:52 +0200

    interactive + polished MLSN chart; 100% line for growth potential chart

--- aux-scripts/deficit-tables.R
+++ aux-scripts/deficit-tables.R
@@ -195,6 +195,7 @@ for(soil_type in soil_types) {
       ggplot(aes(x = month, y = round(growth_potential, 0))) +
       geom_hline(yintercept = 10, color = "#F9D4C9", linewidth = .4) +
       geom_hline(yintercept = 50, color = "#F2F1C6", linewidth = .4) +
+      geom_hline(yintercept = 100, color = "#B5CBA5", linewidth = .4) +
       geom_col(fill = "white", width = .8) +
       geom_col_interactive(aes(fill = condition, data_id = month, tooltip = tooltip), width = .8) +
       geom_hline(yintercept = 0, color = torv_gray_light, linewidth = .6) +
@@ -203,10 +204,10 @@ for(soil_type in soil_types) {
       labs(x = NULL, y = NULL, title = "Monthly Growth Potential") +
       theme(
         panel.grid.major.x = element_blank(),
-        plot.title = element_text(margin = margin(b = 20), size = 12, family =typeface),
+        plot.title = element_text(size = rel(.95), margin = margin(5, 0, 10, 0)),
         plot.title.position = "plot",
-        axis.text.x = element_text(margin = margin(t = 5)),
-        axis.text.y = element_text(size = rel(.8), color = c("#7C230D", "#686617"))
+        axis.text.x = element_text(size = rel(1.2), margin = margin(t = 5)),
+        axis.text.y = element_text(color = c("#7C230D", "#686617", torv_green2))
       )
     
     if(!html) {
@@ -223,24 +224,23 @@ for(soil_type in soil_types) {
           size = 1.9, vjust = -.7, color = torv_gray,
         ) +
         scale_y_continuous(
-          breaks = c(10, 50), labels = scales::percent_format(scale = 1), expand = expansion(add = c(0, 5))
+          breaks = c(10, 50, 100), labels = scales::percent_format(scale = 1), expand = expansion(add = c(0, 5))
         ) +
-        scale_color_manual(values = c("good" = "#3C6504", "stressed" = "#494802", "limited" = "#5F1400"), guide = "none") +
-        theme(plot.margin = margin(15, 1, 5, 1))
+        scale_color_manual(values = c("good" = "#3C6504", "stressed" = "#494802", "limited" = "#5F1400"), guide = "none")
     } else {
       growth_potential_plot <-
         growth_potential_plot +
         scale_y_continuous(
-          breaks = c(10, 50),  labels = scales::percent_format(scale = 1), expand = c(0, 0)
+          breaks = c(10, 50, 100),labels = scales::percent_format(scale = 1), expand = c(0, 0)
         )
     }
 
     ggsave(here(root_figure_location, "monthly_growth_potential_plot.png"), 
-           growth_potential_plot, width = 6.5, height = 4.1, bg = "#ffffff", dpi = 600)
+           growth_potential_plot, width = 6.5, height = 4.1, bg = "white", dpi = 500)
 
     annual_N_per_1000sqft <- sum(climate$N)
     
-    saveRDS(growth_potential_plot, here("report", "figures", "growth_potential_plot.Rds"))
+    saveRDS(growth_potential_plot, here(root_figure_location, "growth_potential_plot.Rds"))
 
   }
 
@@ -252,11 +252,15 @@ for(soil_type in soil_types) {
   climate <- climate %>%
     mutate(month = factor(month, levels = month.abb)) %>%
     summarise(across(N:Mn, sum)) %>%
-    pivot_longer(cols = N:Mn, names_to = "mehlich_3",
-                 values_to = "total_lb_per_1000sqft") %>%
+    pivot_longer(
+      cols = N:Mn, names_to = "mehlich_3",
+      values_to = "total_lb_per_1000sqft"
+    ) %>%
     left_join(mlsn_table, by = "mehlich_3") %>% # specified to reduce console output
-    mutate(removed_from_soil_ppm = total_lb_per_1000sqft * 32.67,
-           plus_mlsn_ppm = removed_from_soil_ppm + ppm) %>%
+    mutate(
+      removed_from_soil_ppm = total_lb_per_1000sqft * 32.67,
+      plus_mlsn_ppm = removed_from_soil_ppm + ppm
+    ) %>%
     mutate(soil_type = soil_type)
 
   climate_all_types <- rbind(climate_all_types, climate)
@@ -305,148 +309,181 @@ deficit_averages <- deficit_analysis %>%
 mixedrank <- function(x) order(gtools::mixedorder(x))
 
 deficit_table_all <- deficit_analysis %>%
-  pivot_wider(id_cols = c(sample_description_number_1, sample_description_number_2),
-              names_from = mehlich_3,
-              values_from = deficit) %>%
+  pivot_wider(
+    id_cols = c(sample_description_number_1, sample_description_number_2),
+    names_from = mehlich_3, values_from = deficit
+  ) %>%
   mutate(Area = as.character(sample_description_number_2)) %>%
   select(-sample_description_number_2) %>%
   # Using mixedrank function to allow for alpha numeric combinations (e.g. "blah 1, blah 2, blah 12")
   arrange(mixedrank(Area)) %>%
-  rbind(pivot_wider(deficit_averages, id_cols = sample_description_number_1,
-                    names_from = mehlich_3,
-                    values_from = deficit) %>%
-          mutate(Area = "Average")) %>%
+  rbind(
+    pivot_wider(
+      deficit_averages, id_cols = sample_description_number_1,
+      names_from = mehlich_3, values_from = deficit
+    ) %>%
+    mutate(Area = "Average")
+  ) %>%
   arrange(sample_description_number_1)
 
 # Turn deficits into fertilizer amount, using formulae in spreadsheet
-fertilizer_table_all <- deficit_table_all %>%
-  mutate(S = -S/33,
-         P = -P*2.29/33,
-         Ca = -Ca/33,
-         Mg = -Mg/33,
-         K = -K*1.2/33,
-         Fe = -Fe/33,
-         Mn = -Mn/33) %>%
+fertilizer_table_all <- 
+  deficit_table_all %>%
+  mutate(
+    S = -S/33,
+    P = -P*2.29/33,
+    Ca = -Ca/33,
+    Mg = -Mg/33,
+    K = -K*1.2/33,
+    Fe = -Fe/33,
+    Mn = -Mn/33
+  ) %>%
   # To keep the order of the elements consisttent across tables
   select(c(sample_description_number_1, Area, setdiff(elemental_ratios$element, "N"))) %>%
-  mutate(across(setdiff(elemental_ratios$element, "N"), function(x) ifelse(is.na(x), NA,
-                                                                           # only keeping values greater than 0
-                                                                           ifelse(x > 0, round(x, 2),
-                                                                                  "—")))) %>%
-  rename('P~2~O~5~' = P,
-         'K~2~O' = K)
+  mutate( # only keeping values greater than 0
+    across(setdiff(elemental_ratios$element, "N"), 
+           function(x) ifelse(is.na(x), NA, ifelse(x > 0, round(x, 2), "—")))
+  ) %>%
+  rename(
+    'P~2~O~5~' = P,
+    'K~2~O' = K
+  )
 
 
 # Turn 0s and positive values ( = no deficit) into '-' to display deficits
 deficit_table_all <- deficit_table_all %>%
   # using ifelse because case_when doesn't accept the change in type (double to character)
-  mutate(across(setdiff(elemental_ratios$element, "N"), function(x) ifelse(is.na(x), NA,
-                                                                           ifelse(x < 0, round(x, 2),
-                                                                                  "—"))))
+  mutate(
+    across(setdiff(elemental_ratios$element, "N"), 
+           function(x) ifelse(is.na(x), NA, ifelse(x < 0, round(x, 2), "—")))
+  )
 
 # Get the data ready to plot and comment on ----
-deficits_graph_data <- deficit_analysis %>%
+deficits_graph_data <- 
+  deficit_analysis %>%
   group_by(sample_description_number_1, sample_description_number_2, mehlich_3) %>%
-  summarise(mean_measurement = mean(measurement_result),
-            aiming_for = unique(plus_mlsn_ppm)) %>%
-  mutate(point_color = case_when(mean_measurement > aiming_for * 1.01 ~ torv_green,
-                                 mean_measurement > aiming_for * .99 ~ torv_orange,
-                                 TRUE ~ "#ec4a35")) %>%
+  summarise(
+    mean_measurement = mean(measurement_result),
+    aiming_for = unique(plus_mlsn_ppm)
+  ) %>%
+  mutate(point_color = case_when(
+    mean_measurement > aiming_for * 1.01 ~ torv_green,
+    mean_measurement > aiming_for * .99 ~ "#FFBF00",
+    TRUE ~ "#B22222")
+  ) %>%
   group_by(sample_description_number_1, mehlich_3) %>%
-  mutate(mean_per_soil_type = mean(mean_measurement),
-         proportion_in_deficit = sum(mean_measurement < aiming_for * 1.01)/
-           length(mean_measurement)*100) %>%
+  mutate(
+    mean_per_soil_type = mean(mean_measurement),
+    proportion_in_deficit = sum(mean_measurement < aiming_for * 1.01) / length(mean_measurement)*100
+  ) %>%
   ungroup() %>%
-  mutate(mean_line_color = case_when(mean_per_soil_type > aiming_for * 1.01 ~ torv_green,
-                                     mean_per_soil_type > aiming_for * .99 ~ torv_orange,
-                                     TRUE ~ "#ec4a35")) %>%
+  mutate(mean_line_color = case_when(
+    mean_per_soil_type > aiming_for * 1.01 ~ torv_green,
+    mean_per_soil_type > aiming_for * .99 ~ "#FFBF00",
+    TRUE ~ "#B22222")
+  ) %>%
   arrange(mixedrank(sample_description_number_2)) %>%
-  mutate(sample_description_number_2 = factor(sample_description_number_2,
-                                              levels = unique(sample_description_number_2)),
-         mehlich_3 = factor(mehlich_3,
-                            levels = c("K", "P", "Ca", "Mg", "S", "Fe", "Mn"),
-                            labels = c("K<sub>2</sub>O", "P<sub>2</sub>O<sub>5</sub>",
-                                       "Ca", "Mg", "S", "Fe", "Mn")))
+  mutate(
+    sample_description_number_2 = factor(sample_description_number_2,
+                                         levels = unique(sample_description_number_2)),
+    mehlich_3 = factor(mehlich_3,
+                       levels = c("K", "P", "Ca", "Mg", "S", "Fe", "Mn"),
+                       labels = c("K<sub>2</sub>O", "P<sub>2</sub>O<sub>5</sub>",
+                                  "Ca", "Mg", "S", "Fe", "Mn")),
+    tooltip = paste0("<span style='font-size:12pt;'>", soil_type, " ", sample_description_number_2, "</span><br><b style=color:", clr_darken(point_color, .3), ";>", mean_measurement, "</b>")
+  )
 
 # Plot MLSN deficits per sample ----
 create_deficits_graph <- function(soil_type) {
 
-  deficit_graph <- ggplot(filter(deficits_graph_data, sample_description_number_1 == soil_type)) +
-    geom_hline(aes(yintercept = aiming_for),
-               color = alpha(torv_orange, .5)) +
-    geom_hline(aes(yintercept = mean_per_soil_type,
-                   color = mean_line_color),
-               linetype = 3) +
-    geom_segment(aes(y = aiming_for, yend = mean_measurement,
-                     x = sample_description_number_2,
-                     xend = sample_description_number_2),
-                 linetype = 1,
-                 linewidth = .3,
-                 color = alpha(torv_orange, .5)) +
-    geom_point(aes(x = sample_description_number_2,
-                   y = mean_measurement,
-                   fill = point_color),
-               shape = 21,
-               color = alpha(torv_orange, .5),
-               size = 2) +
+  deficit_graph <- 
+    ggplot(filter(deficits_graph_data, sample_description_number_1 == soil_type)) +
+    geom_segment_interactive(
+      aes(x = sample_description_number_2,
+          xend = sample_description_number_2,
+          y = aiming_for, yend = mean_measurement, 
+          color = stage(point_color, after_scale = clr_darken(color, .3)),
+          tooltip = tooltip, data_id = mehlich_3),
+      linewidth = .8
+    ) +
+    geom_hline(
+      aes(yintercept = aiming_for),
+      linewidth = .8, color = "grey92"
+    ) +
+    geom_hline_interactive(
+      aes(yintercept = aiming_for,
+          tooltip = paste0("<span style='font-size:12pt;'>MLSN</span><br><b style='color:", clr_darken(torv_orange, .3), ";'>", sprintf("%2.1f", aiming_for), "</b>"), 
+          data_id = mehlich_3),
+      linewidth = .8, color = torv_orange
+    ) +
+    geom_hline_interactive(
+      aes(yintercept = mean_per_soil_type, color = mean_line_color,
+          tooltip = paste0("<span style='font-size:12pt;'>Mean</span><br><b style='color:", clr_darken(mean_line_color, .3), ";'>", sprintf("%2.1f", mean_per_soil_type), "</b>"), 
+          data_id = mehlich_3),
+      linetype = "32", linewidth = .5
+    ) +
+    geom_point(
+      aes(x = sample_description_number_2, y = mean_measurement),
+      color = "grey92", size = 2.5
+    ) +
+    geom_point_interactive(
+      aes(x = sample_description_number_2, y = mean_measurement, 
+          fill = point_color, color = after_scale(clr_darken(fill, .3)),
+          tooltip = tooltip, data_id = mehlich_3),
+      shape = 21, size = 2.5, stroke = .8
+    ) +
+    ggtext::geom_textbox(
+      aes(x = Inf,
+          y = aiming_for,
+          label = glue::glue("<span style=\"font-size:6pt\">MLSN<br></span>**{janitor::round_half_up(aiming_for)}**"),
+          vjust = case_when(aiming_for > mean_per_soil_type ~ .2, TRUE ~ .9),
+          valign = case_when(aiming_for > mean_per_soil_type ~ 0, TRUE ~ 1)),
+      hjust = 1.1, halign = .5, stat = "unique",
+      family = typeface, lineheight = .85, alpha = .8, color = clr_darken(torv_orange, .3), size = 3,
+      box.color = NA, fill = NA, width = unit(2, "lines")
+    ) +
+    ggtext::geom_textbox(
+      aes(x = Inf,
+          y = mean_per_soil_type,
+          label = glue::glue("<span style=\"font-size:6pt\">MEAN<br></span>**{janitor::round_half_up(mean_per_soil_type)}**"),
+          color = stage(mean_line_color, after_scale = clr_darken(color, .3)),
+          vjust = case_when(aiming_for > mean_per_soil_type ~ .9, TRUE ~ .2),
+          valign = case_when(aiming_for > mean_per_soil_type ~ 1, TRUE ~ 0)),
+      hjust = 1.1, halign = .5, stat = "unique",
+      family = typeface, lineheight = .85, alpha = .8, size = 3,
+      box.color = NA, fill = NA, width = unit(2, "lines")
+    ) +
+    facet_wrap(
+      ~ mehlich_3, 
+      ncol = 1, scales = "free_y",
+      strip.position = "left"
+    ) +
+    scale_x_discrete(
+      expand = expansion(mult = c(.05, .2)), position = "top",
+      labels = function(x) glue::glue("<span style=\"font-size:7.5pt\">{soil_type}<br></span>**{x}**")
+    ) +
+    scale_y_continuous(
+      expand = expansion(mult = 1), position = "right"
+    ) +
     scale_fill_identity() +
     scale_color_identity() +
-    labs(x = "",
-         y = "") +
-    ggtext::geom_textbox(aes(x = Inf,
-                             y = aiming_for,
-                             label = glue::glue("<span style=\"font-size:5pt\">MLSN<br></span>**{janitor::round_half_up(aiming_for)}**"),
-                             vjust = case_when(aiming_for > mean_per_soil_type ~ .15,
-                                               TRUE ~ .85),
-                             valign = case_when(aiming_for > mean_per_soil_type ~ 0,
-                                                TRUE ~ 1)),
-                         hjust = 1,
-                         halign = .5,
-                         family = typeface,
-                         lineheight = .85,
-                         color = alpha(torv_orange, .5),
-                         box.color = NA,
-                         fill = NA,
-                         width = unit(2, "lines"),
-                         alpha = .8,
-                         size = 3) +
-    ggtext::geom_textbox(aes(x = Inf,
-                             y = mean_per_soil_type,
-                             label = glue::glue("<span style=\"font-size:5pt\">MEAN<br></span>**{janitor::round_half_up(mean_per_soil_type)}**"),
-                             color = mean_line_color,
-                             vjust = case_when(aiming_for > mean_per_soil_type ~ .85,
-                                               TRUE ~ .15),
-                             valign = case_when(aiming_for > mean_per_soil_type ~ 1,
-                                                TRUE ~ 0)),
-                         hjust = 1.2,
-                         halign = .5,
-                         family = typeface,
-                         lineheight = .85,
-                         box.color = NA,
-                         fill = NA,
-                         width = unit(4, "lines"),
-                         alpha = .8,
-                         size = 3) +
-    facet_wrap(.~mehlich_3, ncol = 1, scales = "free_y",
-               strip.position = "left") +
-    scale_y_continuous(expand = expansion(mult = 1),
-                       position = "right") +
-    scale_x_discrete(expand = expansion(add = c(.5, 2.5)),
-                     position = "top",
-                     labels = function(x)
-                       glue::glue("<span style=\"font-size:5pt\">{soil_type}<br></span>**{x}**")) +
-    theme(strip.background = element_rect(color = torv_orange, fill = torv_orange),
-          panel.grid = element_line(color = "white", linewidth = .3),
-          panel.grid.major.y = element_blank(),
-          panel.background = element_rect(colour = "#FFFFFF", fill = "#EC8C351A"),
-          strip.text.y.left = ggtext::element_markdown(color = "#FFFFFF", angle = 0, face = "bold"),
-          axis.text.y = element_blank(),
-          axis.ticks = element_blank(),
-          axis.text.x.top = ggtext::element_markdown(color = torv_gray, family = typeface),
-          panel.spacing = unit(.75, "lines"))
+    labs(x = NULL, y = NULL) +
+    theme(
+      strip.background = element_rect(color = torv_orange, fill = torv_orange),
+      panel.grid = element_line(color = "white", linewidth = .3),
+      panel.grid.major.y = element_blank(),
+      panel.background = element_rect(color = "grey92", fill = "grey92"),
+      strip.text.y.left = ggtext::element_markdown(color = "white", angle = 0, face = "bold"),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.text.x.top = ggtext::element_markdown(color = torv_gray, family = typeface, size = rel(1.8)),
+      panel.spacing = unit(.65, "lines")
+    )
 
   ggsave(here(root_figure_location, "soil_testing", glue::glue("MLSN_deficits_{soil_type}_plot.png")),
-         deficit_graph, width = 6.5, height = 5.5, bg = "#FFFFFF", dpi = 300)
+         deficit_graph, width = 6.5, height = 6.2, bg = "white", dpi = 500)
+  
+  saveRDS(deficit_graph, here(root_figure_location, "soil_testing", glue::glue("MLSN_deficits_{soil_type}_plot.Rds")))
 }
 
 purrr::map(soil_types, create_deficits_graph)
