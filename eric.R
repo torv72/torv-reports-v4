0c5a35c322fdfba0c430f179cf518610e9791406
torv72 <90870379+torv72@users.noreply.github.com>
2025-04-21 17:57:32 -0600

    Updates to data and minor changes

--- eric.R
+++ eric.R
@@ -11,18 +11,103 @@ source(here::here("aux-scripts/update-database.R"))
 source(here::here("aux-scripts/generate-report.R"))
 
 # ... and now run the function with the custom inputs:
+
 generate_report(
-  .site_name = "CommonGround Golf Course",
-  .site_name_abbr = "48321-commonground",
-  .date_sample_submitted = "2025-02-23",
+  .site_name = "Hemingway Golf Course",
+  .site_name_abbr = "34498-hemingway",
+  .date_sample_submitted = "2025-04-21",
   .start_date = "2017-01-01",
   .om_seasons = "season",
   .om_stats = "average",
   .warm_or_cool = "cool",
   .acid_extract = "Mehlich",
   .include_results_interpretation = "No",
+  .include_sand_fraction = "Yes",
+  .output = c("pdf")
+)
+
+generate_report(
+  .site_name = "CommonGround Golf Course",
+  .site_name_abbr = "48321-commonground",
+  .date_sample_submitted = "2025-04-15",
+  .start_date = "2017-01-01",
+  .om_seasons = "season",
+  .om_stats = "average",
+  .warm_or_cool = "cool",
+  .acid_extract = "Mehlich",
+  .include_results_interpretation = "No",
+  .include_sand_fraction = "Yes",
+  .output = c("pdf")
+)
+
+generate_report(
+  .site_name = "Town of Basalt",
+  .site_name_abbr = "46061-basalt",
+  .date_sample_submitted = "2025-04-17",
+  .start_date = "2017-01-01",
+  .om_seasons = "season",
+  .om_stats = "average",
+  .warm_or_cool = "cool",
+  .acid_extract = "Mehlich",
+  .include_results_interpretation = "Yes",
+  .include_sand_fraction = "No",
+  .output = c("pdf")
+)
+
+generate_report(
+  .site_name = "Town of Basalt",
+  .site_name_abbr = "46061-basalt",
+  .date_sample_submitted = "2025-04-16",
+  .start_date = "2017-01-01",
+  .om_seasons = "season",
+  .om_stats = "average",
+  .warm_or_cool = "cool",
+  .acid_extract = "Mehlich",
+  .include_results_interpretation = "Yes",
+  .include_sand_fraction = "No",
+  .output = c("pdf")
+)
+
+generate_report(
+  .site_name = "Greeley Country Club",
+  .site_name_abbr = "89424-greeley",
+  .date_sample_submitted = "2025-04-15",
+  .start_date = "2017-01-01",
+  .om_seasons = "season",
+  .om_stats = "average",
+  .warm_or_cool = "cool",
+  .acid_extract = "Mehlich",
+  .include_results_interpretation = "Yes",
   .include_sand_fraction = "No",
   .output = c("html","pdf")
+)
+
+generate_report(
+  .site_name = "Crown Mountain Park",
+  .site_name_abbr = "93893-crown",
+  .date_sample_submitted = "2025-04-03",
+  .start_date = "2017-01-01",
+  .om_seasons = "season",
+  .om_stats = "average",
+  .warm_or_cool = "cool",
+  .acid_extract = "Mehlich",
+  .include_results_interpretation = "Yes",
+  .include_sand_fraction = "No",
+  .output = c("pdf")
+)
+
+generate_report(
+  .site_name = "Town of Basalt",
+  .site_name_abbr = "46061-basalt",
+  .date_sample_submitted = "2024-07-26",
+  .start_date = "2017-01-01",
+  .om_seasons = "season",
+  .om_stats = "average",
+  .warm_or_cool = "cool",
+  .acid_extract = "Mehlich",
+  .include_results_interpretation = "Yes",
+  .include_sand_fraction = "No",
+  .output = c("pdf")
 )
 generate_report(
   .site_name = "The Valley Club Irwin",
